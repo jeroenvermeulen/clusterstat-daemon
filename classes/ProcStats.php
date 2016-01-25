c20be7b814a63277246ee5b865f9b3542b98f7eb
Jeroen Vermeulen <info@jeroenvermeulen.eu>
2016-01-25 23:41:19 +0100

    Added wrap fix to more counters, changed to 64 bits

--- classes/ProcStats.php
+++ classes/ProcStats.php
@@ -973,7 +973,7 @@ class ProcStats {
      */
     protected function _wrapFix( $number )
     {
-        $number = $number % pow(2,32);
+        $number = $number % pow(2,64);
         return $number;
     }
 
